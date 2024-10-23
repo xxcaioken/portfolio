5301213049439d49a5d0e3d9f2856dcc644214c6
Caio Wendhausem Kormives <ckormives@tech6group.com>
2024-10-23 01:00:28 -0300

    feat:adjust jquery to works on "prod" environment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-]
+]#usar jquery local
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
