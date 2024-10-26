e6e578e6b0bf52b2b18238d9865ee4ddce0e9a23
Caio Wendhausem Kormives <ckormives@tech6group.com>
2024-10-26 01:03:17 -0300

    feat: create data structure

--- feedback_suggestions/models.py
+++ feedback_suggestions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User  
 
-# Create your models here.
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    type = models.CharField(max_length=50) 
+    message = models.TextField()
+    date = models.DateField(auto_now_add=True)  
+    
+    class Meta:
+        unique_together = ('user', 'message', 'date') 
+
+    def __str__(self):
+        return f"{self.user.username} - {self.type}"
--- feedback_suggestions/views.py
+++ feedback_suggestions/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Feedback
+from .forms import FeedbackForm
 
 def feedback(request):
-    return render(request, 'feedback_suggestions/index.html')
+    form = FeedbackForm()
+    feedbacks = Feedback.objects.all().order_by('-date')
+    feedback_batches = [feedbacks[i:i+3] for i in range(0, len(feedbacks), 3)]
+    return render(request, 'feedback_suggestions/index.html', {'feedback_batches': feedback_batches, 'form': form})
