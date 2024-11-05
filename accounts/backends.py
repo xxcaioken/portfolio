ca7b6f461103483343004decf1349e0ed599e4c5
Caio Wendhausem Kormives <ckormives@tech6group.com>
2024-11-05 06:25:45 -0300

    feat: add new resources and adjust layout on cell

--- accounts/backends.py
+++ accounts/backends.py
@@ -6,17 +6,14 @@ User = get_user_model()
 class EmailBackend(ModelBackend):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        print("EmailBackend foi inicializado")  
 
     def authenticate(self, request, email=None , password=None, **kwargs):
-        print("Método authenticate chamado com:", request, password, kwargs) 
         email = email or kwargs.get('email')
         
         if email is None:
             return None
         try:
             user = User.objects.get(email=email)
-            print("Usuário encontrado:", user)
             if user.check_password(password):
                 return user
         except User.DoesNotExist:
